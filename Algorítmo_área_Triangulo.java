f4e621b4ba7031c5bff45f3834c1627c69fabf87
DeadPerquim <lperquim@gmail.com>
2025-06-10 01:10:32 +0000

    comitando area triangulo

---
+++ Algorítmo_área_Triangulo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Algorítmo_área_Triangulo {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.print("Digite Base");
+        double B = leia.nextDouble();
+    
+        System.out.print("Digite Altura");
+        double H = leia.nextDouble();
+
+        double area = (B*H) / 2;
+    
+        System.out.println("sua área é" + area);
+    
+        entrada.close();
+    }
+}
