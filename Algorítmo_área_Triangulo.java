e0e2951a2cb45b85bf0974176cc99ec86cc85c60
DeadPerquim <lperquim@gmail.com>
2025-06-16 15:17:13 +0000

    corrigindo leia

--- Algorítmo_área_Triangulo.java
+++ Algorítmo_área_Triangulo.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Algorítmo_área_Triangulo {
     public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
         System.out.print("Digite Base:");
-        double B = leia.nextDouble();
+        double B = entrada.nextDouble();
     
         System.out.print("Digite Altura:");
-        double H = leia.nextDouble();
+        double H = entrada.nextDouble();
 
         double area = (B*H) / 2;
     
         System.out.println("sua área é:" + area);
     
-        leia.close();
+        entrada.close();
     }
 }
