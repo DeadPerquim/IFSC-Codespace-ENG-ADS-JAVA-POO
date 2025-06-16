83715f6fa35b703e24b793b6d68403ce78a4ba3e
DeadPerquim <lperquim@gmail.com>
2025-06-16 17:48:10 +0000

    correção denovo

--- Formula_de_bhaskara.java
+++ Formula_de_bhaskara.java
@@ -5,7 +5,7 @@ public class Formula_de_bhaskara {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Insira o valor de a: ");
-        double a = entrada.nextDouble();1
+        double a = entrada.nextDouble();
 
         System.out.println("Insira o valor de b: ");
         double b = entrada.nextDouble();
@@ -13,7 +13,7 @@ public class Formula_de_bhaskara {
         System.out.println("Insira o valor de c: ");
         double c = entrada.nextDouble();
 
-        double delta = ((b*b)-4*a*c);
+        double delta = (b*b-4*a*c);
 
         double x1 = (-b+Math.sqrt(delta))/(2*a);
         double x2 = (-b-Math.sqrt(delta))/(2*a);
