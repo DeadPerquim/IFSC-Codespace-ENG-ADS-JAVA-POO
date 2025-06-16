caef88e2a47ebfa4bb745f5c7cbc22eb266e6ff0
DeadPerquim <lperquim@gmail.com>
2025-06-16 12:30:31 +0000

    corrigindo area triangulo

--- Algorítmo_área_Triangulo.java
+++ Algorítmo_área_Triangulo.java
@@ -4,15 +4,15 @@ public class Algorítmo_área_Triangulo {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
 
-        System.out.print("Digite Base");
+        System.out.print("Digite Base:");
         double B = leia.nextDouble();
     
-        System.out.print("Digite Altura");
+        System.out.print("Digite Altura:");
         double H = leia.nextDouble();
 
         double area = (B*H) / 2;
     
-        System.out.println("sua área é" + area);
+        System.out.println("sua área é:" + area);
     
         leia.close();
     }
